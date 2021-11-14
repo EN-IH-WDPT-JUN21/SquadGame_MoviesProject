30540946a39f467990a3c5e815c8a4f3d29f1c1e
JerzyDan <jdan@10g.pl>
2021-11-14 17:29:09 +0100

    Added basic nav toolbar version

--- movies-project-frontend/src/app/app-routing.module.ts
+++ movies-project-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
+import { CatalogComponent } from './catalog/catalog.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'catalog',
+    component: CatalogComponent
+  },
+  {
+    path: 'playlists',
+    component: PlaylistsComponent
+  },
+  {
+    path: 'my-profile',
+    component: MyProfileComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
